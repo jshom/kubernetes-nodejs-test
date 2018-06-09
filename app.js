466fe04d7105cb91879458c0dbdaab56180ffdb4
Jacob Shomstein <jacob@shomstein.com>
2018-06-09 01:16:45 -0400

    clean up change, and expect deploy on merge with deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000
 
 app.get('*', (req, res) => {
-  res.send('<b>Would you look at that</b>')
+  res.send('<b>Would you look at that, I think it works</b>')
 })
 
 app.listen(port)
